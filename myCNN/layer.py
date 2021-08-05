b821c5e1ddebd0653935d4d63325d240e0398089
Gursharan Singh <ggurs096@uottawa.ca>
2021-08-05 12:49:06 -0400

    adding myCNN.Relu and myCNN.MaxPool

--- myCNN/layer.py
+++ myCNN/layer.py
@@ -11,4 +11,25 @@ class Padding:
         return padded_data
 
 class Relu:
-    def __init__(self):
+    def __call__(self, data):
+        # element-wise maximum comparison
+        return np.maximum(data, 0)
+
+class MaxPool:
+    def __init__(self, pool_size = 2):
+        self.pool_size = pool_size
+    def __call__(self, data):
+        img_height = data.shape[0]
+        img_width = data.shape[1]
+        new_img_height = int(img_height/self.pool_size)
+        new_img_width = int(img_width/self.pool_size)
+        pooled_data = np.zeros((new_img_height, new_img_width)) #creating an array to store pooled/sampled data
+
+        #selecting the porition of the rectified data
+        for i in range (new_img_height):
+            h_index = self.pool_size*i
+            for j in range (new_img_width):
+                w_index = self.pool_size*j
+                # storing maximum value
+                pooled_data[i][j] = np.max(data[h_index: h_index+self.pool_size, w_index: w_index+self.pool_size])
+        return pooled_data
